7ac56d0039ee35bf36704f28c16adbb0bfbc3311
vanshika parashar <parasharvanshika2005@gmail.com>
2026-06-15 12:59:23 +0530

    arrays level 1 added

---
+++ core_java_practice/gcr_codebase/arrays/level1/fizzuzz.java
@@ -0,0 +1,35 @@
+package gcr_codebase.arrays.level1;
+
+import java.util.Scanner;
+
+public class fizzuzz {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+
+        if (number < 0) {
+            System.out.println("Invalid input");
+            return;
+        }
+
+        String[] result = new String[number + 1];
+
+        for (int i = 0; i <= number; i++) {
+            if (i % 15 == 0)
+                result[i] = "FizzBuzz";
+            else if (i % 3 == 0)
+                result[i] = "Fizz";
+            else if (i % 5 == 0)
+                result[i] = "Buzz";
+            else
+                result[i] = String.valueOf(i);
+        }
+
+        for (int i = 0; i < result.length; i++) {
+            System.out.println("Position " + i + " = " + result[i]);
+        }
+
+        sc.close();
+    }
+    
+}
