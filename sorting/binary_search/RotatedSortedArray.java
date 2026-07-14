602f131675ea9e77cf9d985b2dd6a45eb174cc81
vanshika parashar <parasharvanshika2005@gmail.com>
2026-07-14 09:59:08 +0530

    added assignment

---
+++ sorting/binary_search/RotatedSortedArray.java
@@ -0,0 +1,41 @@
+package binarysearch;
+
+import java.util.*;
+
+public class RotatedSortedArray {
+    static int search(int[] arr, int target) {
+        int low = 0;
+        int high = arr.length - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (arr[mid] == target) {
+                return mid;
+            }
+            if (arr[low] <= arr[mid]) {
+                if (target >= arr[low] && target < arr[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            } else {
+                if (target > arr[mid] && target <= arr[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int target = sc.nextInt();
+        System.out.println(search(arr, target));
+        sc.close();
+    }
+}
